9f3f3eae59e0d60687180a863bf8f182283f7caf
Christian Bustos <christianbus37@gmail.com>
2024-05-17 10:08:57 -0300

    juego terminado

--- js/eventos.js
+++ js/eventos.js
@@ -4,37 +4,44 @@ let y = 0;
 
 export function moveBall(e, ball, stage) {
   const $ball = d.querySelector(ball),
-    $stage = d.querySelector(stage);
+    $stage = d.querySelector(stage),
+    limitsBall = $ball.getBoundingClientRect(),
+    limitsStage = $stage.getBoundingClientRect();
   console.log(e.keyCode);
   console.log(e.key);
-
-  //   const move = (direction) => {
-
-  //   };
 
   switch (e.keyCode) {
     case 37:
       e.preventDefault();
-      //   move("left");
-      x--;
+
+      if (limitsBall.left > limitsStage.left) {
+        x--;
+      }
+
       break;
 
     case 38:
       e.preventDefault();
-      //   move("up");
-      y--;
+      if (limitsBall.top > limitsStage.top) {
+        y--;
+      }
+
       break;
 
     case 39:
       e.preventDefault();
-      //   move("right");
-      x++;
+      if (limitsBall.right < limitsStage.right) {
+        x++;
+      }
+
       break;
 
     case 40:
       e.preventDefault();
-      //   move("down");
-      y++;
+
+      if (limitsBall.bottom < limitsStage.bottom) {
+        y++;
+      }
       break;
 
     default:
